509ad9a6f200c2f69ae5e89c23ae46c5d55435d5
John Crispin <john@phrozen.org>
2025-03-26 12:07:07 +0300

    mediatek: filogic: add Asus ZenWiFi BT8
    Hardware
    --------
    MediaTek MT7988D SoC (3x Cortex-A73 @1.8 GHz max)
    1GB DDR4 RAM
    128MB SPI-NAND (Winbond)
    MediaTek MT7996 BE14000 Tri-Band Wi-Fi 7
    3x LAN (2x 1GE MT7988 built-in, 1x 2.5GE MaxLinear GPY211C)
    1x WAN (2.5GE MT7988 built-in)
    LED: RGB PWM (supported as 3x PWM LED)
    USB: 1x USB 3
    Buttons: RESET, WPS
    UART: 115200 8N1 3.3V
    
    Installation
    ------------
    1. Hold down RESET button and power on the device until
       LED pulses red.
    
    2. Assign IP 192.168.1.70/24 to your computer's Ethernet port
    
    3. Connect Ethernet to one of the 1GE LAN ports
    
    4. Open browser and visit http://192.168.1.1
    
    5. Upload openwrt-mediatek-filogic-asus_zenwifi-bt8-factory.bin
    
    6. Once OpenWrt initramfs system comes up, do sysupgrade using
       openwrt-mediatek-filogic-asus_zenwifi-bt8-squashfs-sysupgrade.bin
    
    Signed-off-by: John Crispin <john@phrozen.org>
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -119,7 +119,8 @@ platform_do_upgrade() {
 	asus,rt-ax57m|\
 	asus,rt-ax59u|\
 	asus,tuf-ax4200|\
-	asus,tuf-ax6000)
+	asus,tuf-ax6000|\
+	asus,zenwifi-bt8)
 		CI_UBIPART="UBI_DEV"
 		CI_KERNPART="linux"
 		nand_do_upgrade "$1"
@@ -244,7 +245,8 @@ platform_pre_upgrade() {
 	asus,rt-ax57m|\
 	asus,rt-ax59u|\
 	asus,tuf-ax4200|\
-	asus,tuf-ax6000)
+	asus,tuf-ax6000|\
+	asus,zenwifi-bt8)
 		asus_initial_setup
 		;;
 	xiaomi,mi-router-ax3000t|\
